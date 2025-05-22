6a92b77237f823fa3d2064eec5f1b003883279fc
Dipak-Natureglobal <dipak.mourya@natureglobal.com>
2025-05-22 18:56:10 +0530

    feat: update metadata and add Open Graph support for portfolio pages

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,22 @@
 export const metadata = {
   title: "Portfolio | Services",
-  description: "Explore the services provided in the portfolio.",
+  description: "Welcome to the portfolio of Dipak, a full-stack web developer showcasing web development projects and design skills.",
+  openGraph: {
+    title: 'Dipak Portfolio | Services',
+    url: 'https://dipakdev.in/services',
+    type: 'website',
+    images: [
+      {
+        url: '/opengraph/profile/services.png',
+        width: 1200,
+        height: 630,
+        alt: 'Dipak Dev Service Image',
+      },
+    ],
+  },
 };
 
-import Services from "../../components/services/index"; 
+import Services from "../../components/services/index";
 
 export default function ServicesPage() {
   return <Services />;
